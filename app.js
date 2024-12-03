db73b7f26ffa5da98cd29f94c40b4cfb9c3b211a
DoriaFabio <doriafabio2001@gmail.com>
2024-12-03 20:23:22 +0100

    try to import router at the app.js

--- app.js
+++ app.js
@@ -17,52 +17,14 @@ const express = require("express");
 const app = express();
 const PORT = 5500;
 
+const postsRouter = require("./routers/posts");
 //definisco dove sono gli asset statici
 app.use(express.static("public")); //http://localhost:5500/
 
-const posts = require("./data/post.js");
 
-//rotta /
-app.get("/", (req, res) => {
-  res.send("<h1>Server del mio blog</h1>");
-});
+//rotte API
+app.use("/posts", postsRouter);
 
-// rotta bacheca
-app.get("/bacheca", (req, res) => {
-  let response = {
-    data: posts,
-    counter: posts.length,
-  };
-  const postTitolo = req.query.titolo;
-  if (postTitolo) {
-    response.data = posts.filter(post => post.titolo.toLowerCase().includes(postTitolo.toLowerCase()));
-    if (response.data.length < 1) {
-      res.status(404);
-      response = {
-        error: 404,
-        message: "Non ci sono post con questa ricerca"
-      }
-    }
-  }
-  res.json(response);
-});
-
-// rotta immagini
-app.get("/ciambellone", (req, res) => {
-  res.send(`<img src="images/ciambellone.jpeg">`);
-});
-app.get("/cracker", (req, res) => {
-  res.send(`<img src="images/cracker_barbabietola.jpeg">`);
-});
-app.get("/pane", (req, res) => {
-  res.send(`<img src="images/pane_fritto_dolce.jpeg">`);
-});
-app.get("/pasta", (req, res) => {
-  res.send(`<img src="images/pasta_barbabietola.jpeg">`);
-});
-app.get("/torta", (req, res) => {
-  res.send(`<img src="images/torta_paesana.jpeg">`);
-});
 
 //rotta fallback
 app.all("*", (req, res) => {
--- routers/posts.js
+++ routers/posts.js
@@ -5,54 +5,54 @@ const blog = require("../data/post.js");  // Tutti i posts
 
 // index
 router.get("/", (req, res) => {
-    const itemName = req.query.titolo;
-    console.log(itemName);
     let response = {
-      count: blog.length,
-      data: [...blog],
-      // copia dell'array nel caso dovessimo filtrare i dati
+        count: blog.length,
+        data: [...blog],
+        // copia dell'array nel caso dovessimo filtrare i dati
     };
     res.json(response);
-  });
-  
-  // leggere una sola pizza - Read one - Show
-  router.get("/:id", (req, res) => {
-    //pizzas/1/
+});
+
+// leggere un solo post - Read one - Show
+router.get("/:id", (req, res) => {
+    //posts/1/
     console.log(req.params);
     const id = parseInt(req.params.id);
     const item = blog.find((item) => item.id === id);
     if (item) {
-      res.json({
-        success: true,
-        item,
-      });
+        res.json({
+            success: true,
+            item,
+        });
     } else {
-      res.status(404);
-      res.json({
-        success: false,
-        message: `Il post ${id} non esiste`,
-      });
+        res.status(404);
+        res.json({
+            success: false,
+            message: `Il post ${id} non esiste`,
+        });
     }
-  });
-  
-  //Create - Store
-  router.post("/", (req, res) => {
+});
+
+//Create - Store
+router.post("/", (req, res) => {
     res.send("Creazione nuovo post");
-  });
-  
-  //Update totale - Update
-  router.put("/:id", (req, res) => {
+});
+
+//Update totale - Update
+router.put("/:id", (req, res) => {
     res.send(`Modifica integrale del post ${id}`);
-  });
-  
-  //Update parziale - Modify
-  router.patch("/:id", (req, res) => {
+});
+
+//Update parziale - Modify
+router.patch("/:id", (req, res) => {
     res.send(`Modifica parziale del post ${id}`);
-  });
-  
-  //Delete (cancellazione) - Destroy
-  router.delete("/:id", (req, res) => {
+});
+
+//Delete (cancellazione) - Destroy
+router.delete("/:id", (req, res) => {
     res.send(`Cancellazione del post ${id}`);
-  });
-  
-  module.exports = router;
+});
+
+
+
+module.exports = router;
